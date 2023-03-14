957c6806cbbd6103ca89440e2262b675032eefc9
Grahambone37 <electricgrunt37@gmail.com>
2023-03-13 21:19:18 -0700

    added band get and post route

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -11,4 +11,25 @@ bands.get('/', async (req, res) => {
     }
 })
 
+bands.post('/', async (req, res) => {
+    try {
+        const newBand = await Band.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new band',
+            data: newBand
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+bands.get('/:id', async (req, res) => {
+    try {
+        const foundBand = await Band.findOne({ where: { band_name: req.params.id } })
+        res.status(200).json(foundBand)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 module.exports = bands
